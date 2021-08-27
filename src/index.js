016419e1336cfd6a2dee17b6d2eaa048b843dc2d
Brendo Silva <breendo.01@gmail.com>
2021-08-27 13:16:05 -0300

    middleware de verificaçao do cpf

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ const customers = [];
 
 app.use(express.json());
 
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).send({ error: "Customer not found" });
+    }
+
+    request.customer = customer;
+
+    return next();
+}
 
 
 app.post("/account", (request, response) => {
@@ -26,14 +39,8 @@ app.post("/account", (request, response) => {
     return response.status(201).send("ok");
 });
 
-app.get("/statement/:cpf", (request, response) => {
-    const { cpf } = request.params;
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if (!customer) {
-        return response.status(400).send({ error: "Customer not found" });
-    }
-
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
     return response.json(customer.statement);
 });
 
